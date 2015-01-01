589959160714b825d9a91734019563b00c1343fa
vvakame <vvakame+dev@gmail.com>
2015-01-01 13:08:02 +0900

    デプロイスクリプトの修正

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -11,15 +11,20 @@ fi
 
 # GITHUB_TOKEN はCircle CIの画面から登録(ないしょ)
 # clone
+echo "cloning..."
 git clone --quiet https://${GITHUB_TOKEN}@github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}.git pages
 cd pages
 git checkout -b gh-pages origin/gh-pages
 
 # update
+echo "updating..."
 rm -rf *.html *.css images/
 cp -r ../public/** ./
 
 # commit & push
+echo "publishing..."
 git add .
 git commit -a -m "build ${CIRCLE_BUILD_NUM} commit:${CIRCLE_SHA1}"
+set +e # 変更ファイル無しの場合コケるけど無視する
 git push --quiet https://${GITHUB_TOKEN}@github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}.git gh-pages
+set -e
