a35ae8e8dadad038d308652e5c5b2dad0dbc4cc0
vvakame <vvakame+dev@gmail.com>
2017-06-25 20:06:17 +0900

    prhの参照の仕方を変更

--- setup.sh
+++ setup.sh
@@ -6,4 +6,4 @@ rm -rf node_modules
 # --unsafe-perm はrootでの実行時(= docker環境)で必要 非root時の挙動に影響なし
 npm install --unsafe-perm
 
-curl -L https://github.com/vvakame/prh/raw/master/misc/techbooster.yml > articles/prh-techbooster.yml
+git submodule init && git submodule update
